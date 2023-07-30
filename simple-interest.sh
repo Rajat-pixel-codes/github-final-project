ad4ad254dd58787dd90ffc454ee30a76a2212b3e
Rajat-pixel-codes <65059677+Rajat-pixel-codes@users.noreply.github.com>
2023-07-30 14:54:06 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,11 +1,4 @@
-   #!/bin/bash
-   # This script calculates simple interest given principal,
-   # annual rate of interest and time period in years.
 
-   # Do not use this in production. Sample purpose only.
-
-   # Author: Upkar Lidder (IBM)
-   # Additional Authors:
    # <Rajat-pixel-codes>
 
    # Input:
